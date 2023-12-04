e17d58ef2fcf8c666404e684686b177d4b364ac5
Pedro-CAB <up202006086@up.pt>
2023-12-04 15:05:28 +0000

    Login Implemented

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Check for existing user session on page load
+    checkUserSession();
+});
+
 function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     var messageElement = document.getElementById('message');
 
-    // Replace this with your actual user authentication logic
-    if (username === 'example' && password === 'password') {
-        messageElement.innerHTML = 'Login successful!';
-        // Redirect to another page or perform other actions after successful login
-    } else {
-        messageElement.innerHTML = 'Invalid username or password';
+    // Send login credentials to the server for validation
+    fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            // Save user session data locally
+            sessionStorage.setItem('userId', data.userId);
+            sessionStorage.setItem('username', data.username);
+
+            // Redirect to another page or perform other actions after successful login
+            window.location.href = '/pages/myLists.html';
+        } else {
+            messageElement.innerHTML = 'Invalid username or password';
+        }
+    })
+    .catch(error => console.error('Login error:', error));
+}
+
+function checkUserSession() {
+    // Check if user session exists
+    const userId = sessionStorage.getItem('userId');
+    const username = sessionStorage.getItem('username');
+
+    if (userId && username) {
+        // User session exists, perform actions accordingly
+        console.log('User is logged in:', username);
+        // Add logic to customize the UI for a logged-in user
+
+        // Update the object list on the page if listsElement is found
+        const listsElement = document.getElementById('lists');
+        if (listsElement) {
+            updateObjectList();
+        }
     }
 }
 
-// Example array of objects
-var objectArray = [
-    { name: 'Lista do Supermercado' },
-    { name: 'Materiais Artísticos' },
-    { name: 'Compras para o Natal' },
-    // ... add more objects as needed
-];
+function logout() {
+    // Clear user session data
+    sessionStorage.removeItem('userId');
+    sessionStorage.removeItem('username');
 
-// Function to update the object list on the page
-function updateObjectList() {
-    var objectListElement = document.getElementById('objectList');
-    objectListElement.innerHTML = ''; // Clear existing list
-
-    // Iterate through the array and create list items
-    objectArray.forEach(function(object) {
-        var listItem = document.createElement('li');
-        listItem.textContent = object.name;
-        objectListElement.appendChild(listItem);
-    });
-}
-
-// Initial update when the page loads
-updateObjectList();
-
-// Example: Add a new object to the array
-function addObject() {
-    var newObjectName = prompt('Enter the name of the new object:');
-    if (newObjectName) {
-        objectArray.push({ name: newObjectName });
-        updateObjectList(); // Update the list after adding a new object
-    }
+    // Redirect to the login page
+    window.location.href = '/pages/login.html';
 }
 
 // Example array of objects
@@ -53,50 +67,46 @@ var objectArray = [
     // ... add more objects as needed
 ];
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the listId from the URL parameter
-    var urlParams = new URLSearchParams(window.location.search);
-    var listId = urlParams.get('id');
+// Function to update the object list on the page
+function updateObjectList() {
+    // Get the listsElement again as it might not be present on every page
+    var listsElement = document.getElementById('lists');
 
-    // Find the object in the array with the matching listId
-    var selectedObject = objectArray.find(function(object) {
-        return object.listId == listId;
-    });
+    if (listsElement) {
+        // Clear existing list
+        listsElement.innerHTML = '';
 
-    if (selectedObject) {
-        // Update the title of the page with the object's name
-        document.getElementById('listTitle').textContent = selectedObject.name;
-
-        // Update the list of items on the page
-        var itemListElement = document.getElementById('itemList');
-        selectedObject.items.forEach(function(item) {
+        // Iterate through the array and create list items
+        objectArray.forEach(function (object) {
             var listItem = document.createElement('li');
-            listItem.textContent = item.itemName;
-            itemListElement.appendChild(listItem);
+            listItem.textContent = object.name;
+            listsElement.appendChild(listItem);
         });
-    } else {
-        // Display an error message if the listId is not found
-        document.getElementById('listTitle').textContent = 'List Not Found';
     }
-});
+}
 
+// Initial update when the page loads
+updateObjectList();
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const listsElement = document.getElementById('lists');
-  
+
     // Fetch shopping lists from the server
     fetch('/api/lists')
-      .then(response => response.json())
-      .then(data => {
-        data.forEach(list => {
-          const listItem = document.createElement('li');
-          listItem.textContent = list.listName;
-          listItem.addEventListener('click', () => {
-            // Implement logic to navigate to the list details page
-          });
-          listsElement.appendChild(listItem);
-        });
-      })
-      .catch(error => console.error('Error fetching lists:', error));
-  });
-  
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(list => {
+                const listItem = document.createElement('li');
+                listItem.textContent = list.listName;
+
+                // Add the event listener to the listItem itself
+                listItem.addEventListener('click', () => {
+                    // Navigate to the itemList.html page with the selected list ID
+                    window.location.href = `/pages/itemList.html?listId=${list.idList}`;
+                });
+
+                listsElement.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Error fetching lists:', error));
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
@@ -8,13 +9,20 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Connect to the SQLite database
-const db = new sqlite3.Database('db.db'); // Replace 'your-database-file.db' with your actual database file name
+// Use session middleware
+app.use(session({
+    secret: 'a2652fc8dc90f14fcfd7b9fe7f79362602ae5da1fb3faad6c261e58f8338a09c',
+    resave: false,
+    saveUninitialized: true,
+}));
 
-// Serve static files from the 'public' directory (adjust based on your directory structure)
+// Connect to the SQLite database
+const db = new sqlite3.Database('db.db');
+
+// Serve static files from the root
 app.use(express.static(path.join(__dirname, '/')));
 
-// Define API endpoints
+// API ENDPOINTS
 
 // Endpoint to get a list of shopping lists
 app.get('/api/lists', (req, res) => {
@@ -31,12 +39,24 @@ app.get('/api/lists', (req, res) => {
 
 // Serve myLists.html
 app.get('/pages/myLists.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/myLists.html'));
+    console.log("Serving myLists.html...")
+    console.log("Checking user authentication...")
+    if (req.session && req.session.userId) {
+        console.log("User is authenticated! Serving...")
+        res.sendFile(path.join(__dirname,'/', '/pages/myLists.html'));
+    } else {
+        console.log("User is not authenticated! Redirecting...")
+        res.redirect('/pages/login.html');
+    }
 });
 
 // Serve itemList.html
 app.get('/pages/itemList.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/itemList.html'));
+    if (req.session && req.session.userId) {
+        res.sendFile(path.join(__dirname, '/', '/pages/itemList.html'));
+    } else {
+        res.redirect('/pages/login.html');
+    }
 });
 
 // Endpoint to get items for a specific shopping list
@@ -55,6 +75,35 @@ app.get('/api/items', (req, res) => {
             res.status(500).send('Internal Server Error');
         } else {
             res.json(rows);
+        }
+    });
+});
+
+// Endpoint to handle user login
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Fetch user from the database based on the provided username
+    const query = 'SELECT * FROM User WHERE username = ?';
+    db.get(query, [username], (err, user) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
+        } else if (!user) {
+            // No user found with the given username
+            res.json({ success: false, message: 'Invalid username or password' });
+        } else {
+            // User found, now compare passwords
+            if (user.password === password) {
+                // Passwords match, store user ID in the session
+                req.session.userId = user.userId;
+
+                // Send success response with user ID and username
+                res.json({ success: true, userId: user.userId, username: user.username });
+            } else {
+                // Passwords do not match
+                res.json({ success: false, message: 'Invalid username or password' });
+            }
         }
     });
 });
